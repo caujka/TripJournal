b92e16821f9efafd6625d5d0b558fff3e6c42c1c
Olesia <olesia.hr@gmail.com>
2014-09-09 10:12:07 +0300

    Edit template

--- TripJournal/settings.py
+++ TripJournal/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django-crossdomainxhr-middleware.XsSharing'
 )
 
 ROOT_URLCONF = 'TripJournal.urls'

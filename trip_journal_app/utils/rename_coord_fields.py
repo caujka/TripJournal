3511e2d522abb0ce1aaf9040ca1fa6abb023f267
Olesia <olesia.hr@gmail.com>
2014-10-15 18:23:48 +0300

    Saving only stories that had been changed. + function for improperly saved markers

--- trip_journal_app/utils/rename_coord_fields.py
+++ trip_journal_app/utils/rename_coord_fields.py
@@ -11,10 +11,23 @@ def rename_coordinates(coordinates):
 def rename_coord_in_stories():
     for story in Story.objects.all():
         text = json.loads(story.text)
+        changed = False
         for block in text:
             if block['marker']:
                 if 'k' in block['marker']:
                     rename_coordinates(block['marker'])
-                    story.text = json.dumps(text)
-                    story.save()
+                    changed = True
+        if changed:
+            story.text = json.dumps(text)
+            story.save()
 
+
+def delete_null_coordinates():
+    for story in Story.objects.all():
+        text = story.text
+        for block in text:
+            # print block['marker']
+            if block['marker'] == {"lat": None, "lng": None}:
+                block['marker'] = None
+        story.text = json.dumps(text)
+        story.save()

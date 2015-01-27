201070ca5f028093a992bb020d6a7f3a5819d97e
Admin <linux@ubuntu.ubuntu-domain>
2015-01-27 23:27:42 +0200

    good

--- trip_journal_app/static/javascript/editVova.js
+++ trip_journal_app/static/javascript/editVova.js
@@ -1,7 +1,25 @@
-
+//Скріпт ,що буде виконуватись після загрузки DOM
 window.onload=function(){
-    first=document.getElementsByTagName("main")[0]
-    first.addEventListener("click", mainClick, false)
+
+var main=document.getElementsByTagName("main")[0];
+ //   first.addEventListener("click", mainClick, false)
+
+//назначаємо функції addEvent, removeEvent в залежності від того чи  підтримує браузер addEventListener (для IE<9 використовуєм attachEvent)
+var addEvent, removeEvent;   
+    if (document.addEventListener) { 
+        addEvent = function(elem, type, handler) {
+            elem.addEventListener(type, handler, false);
+    };
+        removeEvent = function(elem, type, handler) {
+            elem.removeEventListener(type, handler, false);
+    };
+    } else {
+        addEvent = function(elem, type, handler) {
+            elem.attachEvent("on" + type, handler);
+    };
+        removeEvent = function(elem, type, handler) {
+            elem.detachEvent("on" + type, handler);
+    };
 }
 
 //основна хендлер в якому задаеться яка функція визиваеться при якому кліку
@@ -11,12 +29,12 @@ function mainClick(e){
         while(target.id!="content"){
             if(target.id){               
                 switch(target.id){
-                    case "added_artifact": alert("green"); return;
-                    case "added_image": alert("red"); return;
+                  //  case "added_artifact": alert("green"); return;
+                 //   case "added_image": alert("red"); return;
                 }
             }else{
                 switch(target.className){
-                    case "block_title": alert("className"); return;                  
+                  //  case "block_title": alert("className"); return;                  
                 }
             }                                  
             target=target.parentNode;           
@@ -25,12 +43,12 @@ function mainClick(e){
 
 //функція вертає індекс блока по якому клікнули
 function indexOfClickedBlock(element){
-         while (element.className!="my"){
-            element=element.parentNode;
-         }
-         var my=document.getElementsByClassName("my")
-             for(var i=0; i<my.length; i++){
-                if(my[i]==element) return i          
+    while (element.className!="my"){
+        element=element.parentNode;
+    }
+    var my=document.getElementsByClassName("my")
+        for(var i=0; i<my.length; i++){
+            if(my[i]==element) return i          
         } 
 }
 
@@ -38,4 +56,14 @@ function indexOfClickedBlock(element){
 function storyIdFromUrl(){    
     var currUrl = document.URL.split(['/']);
     return currUrl[currUrl.length - 1];
+}
+
+var added_artifact=document.getElementById("added_artifact")
+    addEvent(added_artifact, "click", function(){alert("gogo")})
+
+
+
+
+
+
 }

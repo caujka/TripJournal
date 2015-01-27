00b82cffa503a06678e51a24a48c83336041446d
Admin <linux@ubuntu.ubuntu-domain>
2015-01-27 21:32:03 +0200

    dodav mainClick

--- trip_journal_app/static/javascript/editVova.js
+++ trip_journal_app/static/javascript/editVova.js
@@ -1,13 +1,35 @@
 
 window.onload=function(){
-    s=document.getElementById("story_content")
-    s.onclick=function(){
-        postData(false)
-    }   
+        first=document.getElementById("content")
+        first.addEventListener("click", mainClick, false)
 }
 
+//основна функція яка шукае елемент по якому клікнули і виконує функцію
+function mainClick(e){
+        var event = e || window.event;
+        var target = event.target || event.srcElement;
+        while(target.id!="content"){           
+                switch(target.id){
+                    case "added_artifact": alert("green"); return;
+                    case "added_image": alert("red"); return;           
+            }            
+            target=target.parentNode;           
+        }           
+}
+
+//функція вертає індекс блока по якому клікнули
+function indexOfClickedBlock(element){
+         while (element.className!="my"){
+            element=element.parentNode;
+         }
+         var my=document.getElementsByClassName("my")
+             for(var i=0; i<my.length; i++){
+                if(my[i]==element) return i          
+        } 
+}
+
+//функція вертає з урла браузера story_id
 function storyIdFromUrl(){    
     var currUrl = document.URL.split(['/']);
     return currUrl[currUrl.length - 1];
 }
-

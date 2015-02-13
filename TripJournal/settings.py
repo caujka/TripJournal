9a152eb884f41f32ea4174fb64d07234cb1429f5
ange1os <yaragsap@gmail.com>
2015-02-13 15:47:22 +0200

    deleted db login/password

--- TripJournal/settings.py
+++ TripJournal/settings.py
@@ -14,7 +14,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # local settings
-#import TripJournal.local_settings as local_settings
+import TripJournal.local_settings as local_settings
 
 from utils import client_key_and_secret
 
@@ -75,10 +75,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'TripJournal',
-        # 'USER': local_settings.db_user,
-        # 'PASSWORD': local_settings.db_password,
-        'USER': 'root',
-        'PASSWORD': 'problema',
+        'USER': local_settings.db_user,
+        'PASSWORD': local_settings.db_password,
+        
     }
 }
 
